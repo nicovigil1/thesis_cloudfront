a447a811c4b0906fda246d86c87e2d05dce71822
Nico Vigil <nicovigil1@gmail.com>
2019-09-18 14:01:17 -0400

    adds generator

--- lib/generators/thesis_cloudfront/install_generator.rb
+++ lib/generators/thesis_cloudfront/install_generator.rb
@@ -4,19 +4,8 @@ module ThesisCloudfront
   module Generators
     class InstallGenerator < Rails::Generators::Base
       def code_that_runs
-        create_file 'config/initilizers/asset_host.rb' do 
-          heroku_app_name = ask("what's the name of the heroku app?")
-          "ActionController::Base.asset_host = proc { |_, request|
-            next unless request
-
-            accepted_paths = %w[
-                      /home-social
-                      /take-the-quiz?var=vitamins-protein-explore
-                      /take-the-quiz?var=explore
-                    ]
-
-            ENV['CLOUDFRONT_URL'] if accepted_paths.include?(request.fullpath)
-          }"
+        insert_into_file "Gemfile", after: "group :development, :test do\n" do
+          "gem 'rack_cors'"
         end
       end
     end
