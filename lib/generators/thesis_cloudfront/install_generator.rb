2e9ec2c346f3add52f717994057827fbfb2489b3
Nico Vigil <nicovigil1@gmail.com>
2019-09-18 17:03:35 -0400

    adds initializer to generator

--- lib/generators/thesis_cloudfront/install_generator.rb
+++ lib/generators/thesis_cloudfront/install_generator.rb
@@ -5,7 +5,22 @@ module ThesisCloudfront
     class InstallGenerator < Rails::Generators::Base
       def code_that_runs
         insert_into_file "Gemfile", after: "group :development, :test do\n" do
-          "gem 'rack_cors'"
+          "  gem 'rack_cors'\n"
+        end
+
+        system('bundle')
+
+        create_file "config/initializers/rack_cors.rb" do
+            ["Rails.application.config.middleware.insert_before 0, Rack::Cors do",
+            "  allow do",
+            "    origins [",
+            "      /https:\/\/careoflander.herokuapp.com/,",
+            "      /http:\/\/careoflander.herokuapp.com/,",
+            "      /https:\/\/careoflander-pr-[0-9]+.herokuapp.com/,",
+            "      /http:\/\/careoflander-pr-[0-9]+.herokuapp.com/",
+            "            ]",
+            "  resource '/assets/*', headers: :any, methods: [:get, :head, :options]",
+            "end"].join('\n')
         end
       end
     end
