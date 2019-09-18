e55c3d2555e70ad03a76cd708208219d8e647737
Nico Vigil <nicovigil1@gmail.com>
2019-09-18 18:03:13 -0400

    appends rack cors to gemfile

--- lib/generators/thesis_cloudfront/install_generator.rb
+++ lib/generators/thesis_cloudfront/install_generator.rb
@@ -4,8 +4,8 @@ module ThesisCloudfront
   module Generators
     class InstallGenerator < Rails::Generators::Base
       def install
-        insert_into_file "Gemfile", after: "group :development, :test do\n" do
-          "  gem 'rack_cors'\n"
+        append_to_file "Gemfile" do
+          "\ngem 'rack_cors'"
         end
 
         heroku_app_name = ask("whats the name of this heroku app?")
@@ -13,13 +13,14 @@ module ThesisCloudfront
         create_file "config/initializers/rack_cors.rb" do
             ["Rails.application.config.middleware.insert_before 0, Rack::Cors do",
             "  allow do",
-            "    origins [",
-            "      /https:\\/\\/#{heroku_app_name}.herokuapp.com/,",
-            "      /http:\\/\\/#{heroku_app_name}.herokuapp.com/,",
-            "      /https:\\/\\/#{heroku_app_name}-pr-[0-9]+.herokuapp.com/,",
-            "      /http:\\/\\/#{heroku_app_name}-pr-[0-9]+.herokuapp.com/",
-            "    ]",
-            "  resource '/assets/*', headers: :any, methods: [:get, :head, :options]",
+            "      origins [",
+            "        /https:\\/\\/#{heroku_app_name}.herokuapp.com/,",
+            "        /http:\\/\\/#{heroku_app_name}.herokuapp.com/,",
+            "        /https:\\/\\/#{heroku_app_name}-pr-[0-9]+.herokuapp.com/,",
+            "        /http:\\/\\/#{heroku_app_name}-pr-[0-9]+.herokuapp.com/",
+            "      ]",
+            "    resource '/assets/*', headers: :any, methods: [:get, :head, :options]",
+            "  end",
             "end"].join("\n")
         end
 
