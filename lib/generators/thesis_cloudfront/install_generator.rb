e86461652ceae9f53994d5b745930d5f9f34df55
Nico Vigil <nicovigil1@gmail.com>
2019-09-18 17:07:04 -0400

    adds better generator

--- lib/generators/thesis_cloudfront/install_generator.rb
+++ lib/generators/thesis_cloudfront/install_generator.rb
@@ -11,13 +11,14 @@ module ThesisCloudfront
         system('bundle')
 
         create_file "config/initializers/rack_cors.rb" do
+            heroku_app_name = ask("whats the name of this heroku app?")
             ["Rails.application.config.middleware.insert_before 0, Rack::Cors do",
             "  allow do",
             "    origins [",
-            "      /https:\/\/careoflander.herokuapp.com/,",
-            "      /http:\/\/careoflander.herokuapp.com/,",
-            "      /https:\/\/careoflander-pr-[0-9]+.herokuapp.com/,",
-            "      /http:\/\/careoflander-pr-[0-9]+.herokuapp.com/",
+            "      /https:\/\/#{heroku_app_name}.herokuapp.com/,",
+            "      /http:\/\/#{heroku_app_name}.herokuapp.com/,",
+            "      /https:\/\/#{heroku_app_name}-pr-[0-9]+.herokuapp.com/,",
+            "      /http:\/\/#{heroku_app_name}-pr-[0-9]+.herokuapp.com/",
             "            ]",
             "  resource '/assets/*', headers: :any, methods: [:get, :head, :options]",
             "end"].join('\n')
