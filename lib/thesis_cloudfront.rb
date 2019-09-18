d7bdc530c625bbb998f71187000694824dbae9eb
Nico Vigil <nicovigil1@gmail.com>
2019-09-18 13:33:54 -0400

    initial commit

---
+++ lib/thesis_cloudfront.rb
@@ -0,0 +1,6 @@
+require "thesis_cloudfront/version"
+
+module ThesisCloudfront
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/thesis_cloudfront_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ThesisCloudfront do
+  it "has a version number" do
+    expect(ThesisCloudfront::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
