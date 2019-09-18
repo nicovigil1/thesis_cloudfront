85f115d59728ef433464270612d7bf697442f209
Nico Vigil <nicovigil1@gmail.com>
2019-09-18 17:45:58 -0400

    adds asset host initializer

--- lib/generators/thesis_cloudfront/install_generator.rb
+++ lib/generators/thesis_cloudfront/install_generator.rb
@@ -8,9 +8,9 @@ module ThesisCloudfront
           "  gem 'rack_cors'\n"
         end
 
+        heroku_app_name = ask("whats the name of this heroku app?")
 
         create_file "config/initializers/rack_cors.rb" do
-            heroku_app_name = ask("whats the name of this heroku app?")
             ["Rails.application.config.middleware.insert_before 0, Rack::Cors do",
             "  allow do",
             "    origins [",
@@ -18,9 +18,13 @@ module ThesisCloudfront
             "      /http:\\/\\/#{heroku_app_name}.herokuapp.com/,",
             "      /https:\\/\\/#{heroku_app_name}-pr-[0-9]+.herokuapp.com/,",
             "      /http:\\/\\/#{heroku_app_name}-pr-[0-9]+.herokuapp.com/",
-            "      ]",
+            "    ]",
             "  resource '/assets/*', headers: :any, methods: [:get, :head, :options]",
             "end"].join("\n")
+        end
+
+        create_file "config/initializers/asset_host.rb" do
+          "ActionController::Base.asset_host = ENV['CLOUDFRONT_URL']"
         end
       end
     end
